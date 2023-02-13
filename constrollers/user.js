e9283d008dec16cab06aa1466b1dc45c941eb0c3
mcnadodls <mcnadodls@yandex.ru>
2023-02-13 22:07:58 +0300

    fix saved movies

--- constrollers/user.js
+++ constrollers/user.js
@@ -84,7 +84,7 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.signout = (req, res) => {
-  res.setHeader('Set-Cookie', [`jwt=1;  Path=/;HttpOnly; maxAge=0;SameSite=None;Secure=true;`]);
+  res.setHeader('Set-Cookie', [`jwt=null;  Path=/;HttpOnly; maxAge=0;SameSite=None;Secure=true;`]);
   // res.cookie('jwt', '', {
   //   maxAge: 0,
   //   httpOnly: true,
