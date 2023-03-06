f525102c2d53d22094b11797179efb6a490970c3
mcnadodls <mcnadodls@yandex.ru>
2023-03-06 16:47:20 +0300

    again

--- controllers/users.js
+++ controllers/users.js
@@ -68,20 +68,19 @@ module.exports.createUser = (req, res, next) => {
     });
 };
 
-module.exports.signin = (req, res, next) => {
+module.exports.login = (req, res, next) => {
   const { password, email } = req.body;
 
-  User.findUserByCredentials(email, password) // кастомный метод
+  User.findUserCredentials(email, password) // кастомный метод
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : devSecurityKey, { expiresIn: '7d' }); // Создаем токен
-      res.cookie('jwt', token, { // Передаем токен юзеру
-        maxAge: 3600000 * 24 * 7, // 7 дней срок
-        // httpOnly: true, // из js закрыли доступ
-        sameSite: true, // посылать если запрос сделан с того же домена
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : secretKey, { expiresIn: '7d' }); // Создаем токен
+      res.cookie('jwt', token, { // Передаем токен
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
       });
-      // Изменяем user из JSON в JSObj и удаляем поле пароля
-      const userObj = user.toObject();
-      delete userObj.password;
+      const userObj = user.toObject(); // преобразуем user в объект
+      delete userObj.password; // не возвращаем пароль
       res.send(userObj);
     })
     .catch(next);
@@ -95,3 +94,31 @@ module.exports.signout = (req, res) => {
   });
   res.send({ message: 'Complete' });
 };
+
+// module.exports.signin = (req, res, next) => {
+//   const { password, email } = req.body;
+
+//   User.findUserByCredentials(email, password) // кастомный метод
+//     .then((user) => {
+//       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : devSecurityKey, { expiresIn: '7d' }); // Создаем токен
+//       res.cookie('jwt', token, { // Передаем токен юзеру
+//         maxAge: 3600000 * 24 * 7, // 7 дней срок
+//         // httpOnly: true, // из js закрыли доступ
+//         sameSite: true, // посылать если запрос сделан с того же домена
+//       });
+//       // Изменяем user из JSON в JSObj и удаляем поле пароля
+//       const userObj = user.toObject();
+//       delete userObj.password;
+//       res.send(userObj);
+//     })
+//     .catch(next);
+// };
+
+// module.exports.signout = (req, res) => {
+//   res.cookie('jwt', '', {
+//     maxAge: 0,
+//     httpOnly: true,
+//     sameSite: true,
+//   });
+//   res.send({ message: 'Complete' });
+// };
