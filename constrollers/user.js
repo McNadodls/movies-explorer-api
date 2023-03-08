8e3d38d5b6910375a1bee8f909ebe0509572d166
mcnadodls <mcnadodls@yandex.ru>
2023-03-08 09:54:25 +0300

    again

--- constrollers/user.js
+++ constrollers/user.js
@@ -69,13 +69,13 @@ module.exports.login = (req, res, next) => {
   User.findUserCredentials(email, password) // кастомный метод
   .then((user) => {
     const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : secretKey, { expiresIn: '7d' }); // Создаем токен
-    res.setHeader('Set-Cookie',[`jwt=${token}; Path=/; HttpOnly; maxAge=86400000; SameSite=None; Secure=true;`]);
-    // res.cookie('jwt', token, { // Передаем токен юзеру
-    //   maxAge: 3600000 * 24 * 7, // 7 дней срок
-    //   // httpOnly: true, // из js закрыли доступ
-    //   sameSite: true, // посылать если запрос сделан с того же домена
-    // });
-    // // Изменяем user из JSON в JSObj и удаляем поле пароля
+    // res.setHeader('Set-Cookie',[`jwt=${token}; Path=/; HttpOnly; maxAge=86400000; SameSite=None; Secure=true;`]);
+    res.cookie('jwt', token, { // Передаем токен юзеру
+      maxAge: 3600000 * 24 * 7, // 7 дней срок
+      // httpOnly: true, // из js закрыли доступ
+      sameSite: true, // посылать если запрос сделан с того же домена
+    });
+    // Изменяем user из JSON в JSObj и удаляем поле пароля
     const userObj = user.toObject();
     delete userObj.password;
     res.send(userObj);
